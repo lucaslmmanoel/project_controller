fab22718f5f9f00a1d5c1a1b9dfc0501eb2ce757
Douglas <douglasantana007@gmail.com>
2018-11-17 11:41:42 -0200

    [ADD] Crud de Projeto.
    Adicionado campos do formulário.

--- resources/views/projeto/form.blade.php
+++ resources/views/projeto/form.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="content">
         <div class="row text-center">
-            <div class="col-lg-12">
+            <div class="col-lg-8 offset-lg-2">
                 <div class="card card-stats">
                     <div class="card-header">
                         <div class="col-lg-12">
@@ -22,18 +22,36 @@
                                         <div id="oculto">
                                             <input type="number" name="id_perfil" hidden>
                                         </div>
-                                        <div class="col-lg-10 offset-lg-4">
+                                        <div class="col-lg-10 offset-lg-1">
                                             <label for="tx_nome">Nome<span class="obrigatorio">*</span></label>
                                             <input type="text" class="form-control" name="nome" id="tx_nome"
                                                    maxlength="30" required
                                                    placeholder="Informe um nome ou apelido ao seu projeto"/>
                                         </div>
-                                        <div class="col-lg-10 offset-lg-4">
+                                        <div class="col-lg-10 offset-lg-1">
+                                            <label for="nu_money">Valor<span class="obrigatorio">*</span></label>
+                                            <input type="text" class="form-control" name="valor" id="nu_money"
+                                                   maxlength="30" required />
+                                        </div>
+                                        <div class="col-lg-10 offset-lg-1">
+                                            <label for="dat_inicio">Data de início<span class="obrigatorio">*</span></label>
+                                            <input type="date" class="form-control" name="dt_inicio" id="dat_inicio" required />
+                                        </div>
+                                        <div class="col-lg-10 offset-lg-1">
+                                            <label for="dat_fim">Data Fim<span class="obrigatorio">*</span></label>
+                                            <input type="date" class="form-control" name="dt_fim" id="dat_fim" required />
+                                        </div>
+
+                                        <div class="col-lg-10 offset-lg-1">
                                             <label for="tp_status">Status <span class="obrigatorio">*</span></label>
                                             <input type="radio" class="form-control" name="status" id="tp_status"
                                                    value="A"/> <span class="text-success"> Ativo </span>
                                             <input type="radio" class="form-control" name="status" id="tp_status"
                                                    value="I"/> <span class="text-warning"> Inativo </span>
+                                            <input type="radio" class="form-control" name="status" id="tp_status"
+                                                   value="C"/> <span class="text-warning"> Concluído </span>
+                                            <input type="radio" class="form-control" name="status" id="tp_status"
+                                                   value="P"/> <span class="text-warning"> Pendente </span>
                                             <br>
                                         </div>
                                         <div class="col-lg-12 offset-lg-3">
