2c2812ccdfc4f186009a5de912de3addd978bef0
Douglasx <douglasantana007@gmail.com>
2018-11-19 14:17:37 -0200

    [ADD] ACL.
    Implementando nível de acesso de usuário no sistema. Página de perfil sendo acessada somente por perfil principal.

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -15,12 +15,28 @@ class UsersSeeder extends Seeder
         User::create([
             'name'      => 'Douglasx',
             'email'     => 'd@d.com',
+            'id_perfil' => '1',
             'password'  => bcrypt(123123),
         ]);
 
         User::create([
             'name'      => 'Worldpsych',
             'email'     => 'l@l.com',
+            'id_perfil' => '1',
+            'password'  => bcrypt(123123),
+        ]);
+
+        User::create([
+            'name'      => 'Teste Gerente',
+            'email'     => 't@t.com',
+            'id_perfil' => '2',
+            'password'  => bcrypt(123123),
+        ]);
+
+        User::create([
+            'name'      => 'Teste Funcionário',
+            'email'     => 'f@f.com',
+            'id_perfil' => '3',
             'password'  => bcrypt(123123),
         ]);
     }
